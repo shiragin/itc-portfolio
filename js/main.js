008436a0b38405da8aea0b7c462748eefb39e86d
Shira Ginat <shiragin@gmail.com>
2022-09-26 19:26:06 +0300

    Finish main page except animation

--- js/main.js
+++ js/main.js
@@ -1,14 +1,39 @@
-// fetch profile image and name from github
-
 const GITHUB_URL = 'https://api.github.com/users/shiragin';
+const codingLanguages = ['HTML', 'CSS', 'JavaScript'];
+
+// HTML elements
+const footer = document.querySelector('.credits');
+const card = document.querySelector('.card');
+const profileImage = document.getElementById('profile-image');
+const profileName = document.getElementById('profile-name');
+
+// fetch profile image and name from github
 
 fetch(GITHUB_URL)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    const profileImage = document.getElementById('profile-image');
-    const profileName = document.getElementById('profile-name');
     profileImage.src = data.avatar_url;
-    profileName.src = data.name;
+    profileName.innerText = data.name;
   });
+
+// create footer with codingLanguages array
+
+function createFooter() {
+  footerContent = `This website has been created with: ${codingLanguages[0]}`;
+  if (codingLanguages.length > 1) {
+    for (i = 1; i < codingLanguages.length - 1; i++) {
+      footerContent += `, ${codingLanguages[i]}`;
+    }
+    footerContent += ` and ${codingLanguages[codingLanguages.length - 1]}`;
+  }
+  footer.innerText = footerContent;
+}
+
+createFooter();
+
+// show card only after img loaded up
+profileImage.addEventListener('load', function () {
+  card.classList.remove('hidden');
+});
