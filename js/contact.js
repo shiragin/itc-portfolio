8a713af8c1ab94bd1b857342005f48231d5a7726
Shira Ginat <shiragin@gmail.com>
2022-09-29 21:45:19 +0300

    Simplify form validation

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,5 @@
+const form = document.querySelector('form');
+const inputs = Array.from(document.querySelectorAll('input'));
 const submitButton = document.querySelector('.submit');
 
 // const firstName = document.getElementById('first-name');
@@ -9,19 +11,34 @@ const requiredFields = Array.from(document.querySelectorAll('[required]'));
 
 submitButton.disabled = true;
 
-let runAgainAt = Date.now() + 100;
-
-function nextFrame() {
-  const now = Date.now();
-  if (runAgainAt >= now) {
-    if (!requiredFields.every(e => e.value)) {
-      submitButton.disabled = true;
-    } else {
-      submitButton.disabled = false;
-    }
-    runAgainAt = now + 100;
+function validateForm() {
+  if (!requiredFields.every(e => e.value)) {
+    submitButton.disabled = true;
+  } else {
+    submitButton.disabled = false;
   }
-  requestAnimationFrame(nextFrame);
 }
 
-nextFrame();
+form.addEventListener('keyup', validateForm);
+
+// inputs.forEach(input => {
+//   console.log(input);
+//   input.addEventListener('change', validateForm);
+// });
+
+// let runAgainAt = Date.now() + 100;
+
+// function nextFrame() {
+//   const now = Date.now();
+//   if (runAgainAt >= now) {
+//     if (!requiredFields.every(e => e.value)) {
+//       submitButton.disabled = true;
+//     } else {
+//       submitButton.disabled = false;
+//     }
+//     runAgainAt = now + 100;
+//   }
+//   requestAnimationFrame(nextFrame);
+// }
+
+// nextFrame();
