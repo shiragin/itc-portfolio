f400a7d57f61730642cb1d7673e6f7de1332180b
Shira Ginat <shiragin@gmail.com>
2022-10-01 16:02:06 +0300

    Fix career section, add city to cities section

--- js/about.js
+++ js/about.js
@@ -23,21 +23,27 @@ const next = document.querySelector('.next');
 const cities = [
   {
     name: 'Tel Aviv',
-    desc: "I've lived in Tel Aviv for most of my life and I know it by heart. I love walking its streets and discovering all its hidden secrets. It's the ugliest little city that I've ever loved.",
-    map: 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d11936.246197136878!2d34.77401800628981!3d32.08019604538603!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sil!4v1664325040174!5m2!1sen!2sil',
+    desc: "I was born in Ramat Ha'Sharon, but my family moved to Tel Aviv when I was six years old. I've lived in this city for most of my life and I feel like I know it by heart. It's so vibrant and alive, there's always something happening no matter the time. True, it's not the most beautiful city, definitely the cleanest, but it's home!",
+    map: 'https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d27046.671421552885!2d34.77676250145792!3d32.07374023668278!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sil!4v1664628238992!5m2!1sen!2sil',
     img: '../img/reiseuhu-unsplash.jpg',
   },
   {
     name: 'Mombasa',
-    desc: "Only one traffic light for the entire city, monkeys are the new stray cats, great Indian food, gorgeous beaches and the kindest people. That's the Kenyan coast!",
+    desc: "After my first daughter was born, we moved to Mombasa for four years. It's a different world! Only one traffic light for the entire city, monkeys are the new stray cats, chapati, sukuma wiki and coconut milk, gorgeous beaches and the kindest people. I still miss the Kenyan coast.",
     map: 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d53607.82635418308!2d39.65141949777973!3d-4.056039643592951!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sil!4v1664404893311!5m2!1sen!2sil',
     img: '../img/harshil-gudka-unsplash.jpg',
   },
   {
     name: 'Stockholm',
-    desc: "Best city ever! So pretty and cool. Secondhand shops, quaint cafes, moomins and ABBA, summer sails and winter ski, there's always something to do here!",
+    desc: "Hands down: best city ever! Stockholm is so pretty with its cobblestoned alleys, little secondhand shops, quaint cafes, moomins and ABBA, summer sails, winter ski and beautiful parks. There's something so calming about this city, a sense of 'mysa', as if everyone is so content simply to be there.",
     map: 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d65156.68902333003!2d18.03442303781927!3d59.313810092704756!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sil!4v1664365618738!5m2!1sen!2sil',
     img: '../img/linus-mimietz-unsplash.jpg',
+  },
+  {
+    name: 'Berkeley',
+    desc: "My sister and her family moved to Berkeley, CA, almost ten years ago. While I'm always happy to visit just to see them again, I've grown to love this city. It's so diverse and multicultural, with its amazing selection of restuarant, bookstores and, of course, creepy Victorian houses",
+    map: 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d23699.96706438139!2d-122.28403276177367!3d37.870782031301495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sil!4v1664628704413!5m2!1sen!2sil',
+    img: '../img/yasamine-june-unsplash.jpg',
   },
 ];
 
